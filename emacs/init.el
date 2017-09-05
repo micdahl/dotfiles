1b767f7065dd3a550f29948ec9045ff7e1b4233e
Michael Dahl <michael.dahl84@gmail.com>
2017-09-05 10:59:58 +0200

    Deleted custom settings

--- emacs/init.el
+++ emacs/init.el
@@ -8,17 +8,3 @@
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil-surround evil-magit use-package evil auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
